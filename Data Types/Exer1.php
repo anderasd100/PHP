d2dbc3b03f9429406d5504a7421458c4db9d3d61
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-04-22 17:36:25 -0400

    Estudo PHP 22.04.2025

---
+++ Data Types/Exer1.php
@@ -0,0 +1,19 @@
+<?php
+
+//Qual é o tipo de dado de um valor booleano em PHP?
+# Quais são os dois valores possíveis para o tipo de dado booleano?
+/*Como você declara uma variável booleana com o valor `true` em PHP? */
+// MC franqueados
+//API
+//POSTGREE
+//kaban
+//ANALISE DE REQUISITOS
+//larawel
+
+$Var1 = true;
+$Var2 = false;
+
+var_dump($Var1);
+var_dump($Var2);
+
+?>
